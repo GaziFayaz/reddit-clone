577312c8894414723eb51ca420bf8d4782bc0be8
gazi_fayaz <gazifayaz.16694@gmail.com>
2023-02-07 00:58:41 +0600

    Change navbar after login and appear logout button

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -12,13 +12,17 @@ import {
   ModalFooter,
   Flex,
   Text,
+  useFocusEffect,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
 import AuthInputs from "./AuthInputs";
 import OAuthButtons from "./OAuthButtons";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
+  const [user, loading, error] = useAuthState(auth)
 
   const handleClose = () => {
     setModalState((prev) => ({
@@ -26,6 +30,12 @@ const AuthModal: React.FC = () => {
       open: false,
     }));
   };
+
+ useEffect(() => {
+  if (user) handleClose()
+  console.log("user", user)
+ }, [user])
+
   return (
     <>
       <Modal isOpen={modalState.open} onClose={handleClose}>
